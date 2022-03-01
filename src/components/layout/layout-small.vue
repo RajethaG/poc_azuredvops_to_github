35dada5ebe7cd804f9df9cfd25f2586fce9b9848
rajetha jagadish <rajetha.jagadish@thoughtfocus.com>
2022-03-01 17:19:40 +0530

    comment fix

--- src/components/layout/layout-small.vue
+++ src/components/layout/layout-small.vue
@@ -1,7 +1,5 @@
 <template>
   <v-container>
-    <!-- <v-app-bar height="90" fixed class="primary" shrink-on-scroll dark> -->
-    <!-- </v-app-bar> -->
     <transition name="fade" mode="out-in">
       <div class="container backgroundColor bordered mt-5 rounded">
         <router-view />
