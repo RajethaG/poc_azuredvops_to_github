3d82b85821c170291abdc4c3f120e169a178d8fe
rajetha jagadish <rajetha.jagadish@thoughtfocus.com>
2022-03-07 20:24:28 +0530

    US 21740

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,11 +2,11 @@
 export default {
   // eslint-disable-next-line max-statements
   downloadFile(fileData, contentType, fileName, forceDownload) {
-    const blob = new Blob([base64ToArrayBuffer(fileData)], {
+    const data = fileData.trim()
+    const blob = new Blob([base64ToArrayBuffer(data)], {
       type: contentType
     })
     const url = window.URL.createObjectURL(blob)
-    const urldata = fileData
     if (forceDownload) {
       // check for support for IE, which will not support blob save via links
       if (window.navigator && window.navigator.msSaveOrOpenBlob) {
@@ -26,7 +26,7 @@ export default {
         window.navigator.msSaveOrOpenBlob(blob, fileName)
       } else {
         const link = document.createElement('a')
-        link.href = urldata
+        link.href = url
         link.click()
       }
     } else {
